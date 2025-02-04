7128aa0e8bf38b38e87502481fa70daa94ebda9f
Jumbox <Jumbox@Jumbox>
2025-02-04 16:45:39 +0800

    added GUI

--- Deliverly/src/com/deliverly/main/customer/CustomerMenu.java
+++ Deliverly/src/com/deliverly/main/customer/CustomerMenu.java
@@ -26,21 +26,260 @@ public class CustomerMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        mainPanel = new javax.swing.JTabbedPane();
+        menuPanel = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        menuList = new javax.swing.JList<>();
+        jLabel3 = new javax.swing.JLabel();
+        deliveryOption = new javax.swing.JComboBox<>();
+        placeorderbutton = new javax.swing.JButton();
+        orderHistoryPanel = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        orderHistoryList = new javax.swing.JList<>();
+        jLabel4 = new javax.swing.JLabel();
+        reviewsList = new javax.swing.JList<>();
+        jLabel5 = new javax.swing.JLabel();
+        reorderButton = new javax.swing.JButton();
+        complainsPanel = new javax.swing.JPanel();
+        complains = new javax.swing.JScrollPane();
+        complainsTextArea = new javax.swing.JTextArea();
+        jLabel6 = new javax.swing.JLabel();
+        submitComplain = new javax.swing.JButton();
+        transactionHistoryPanel = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        transactionTable = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        menuList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(menuList);
+
+        jLabel3.setText("Delivery Options");
+
+        deliveryOption.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Dine-in", "Take-away", "Delivery" }));
+        deliveryOption.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        deliveryOption.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+
+        placeorderbutton.setText("Place Order");
+        placeorderbutton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                placeorderbuttonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout menuPanelLayout = new javax.swing.GroupLayout(menuPanel);
+        menuPanel.setLayout(menuPanelLayout);
+        menuPanelLayout.setHorizontalGroup(
+            menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, menuPanelLayout.createSequentialGroup()
+                .addContainerGap(362, Short.MAX_VALUE)
+                .addGroup(menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(placeorderbutton)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(deliveryOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(282, 282, 282))
+            .addGroup(menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(menuPanelLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 324, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(405, Short.MAX_VALUE)))
+        );
+        menuPanelLayout.setVerticalGroup(
+            menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(menuPanelLayout.createSequentialGroup()
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(deliveryOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(96, 96, 96)
+                .addComponent(placeorderbutton)
+                .addContainerGap(266, Short.MAX_VALUE))
+            .addGroup(menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(menuPanelLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(262, Short.MAX_VALUE)))
+        );
+
+        mainPanel.addTab("Menu", menuPanel);
+
+        orderHistoryList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(orderHistoryList);
+
+        jLabel4.setText("Order History");
+
+        reviewsList.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+
+        jLabel5.setText("Reviews");
+
+        reorderButton.setText("Reorder");
+
+        javax.swing.GroupLayout orderHistoryPanelLayout = new javax.swing.GroupLayout(orderHistoryPanel);
+        orderHistoryPanel.setLayout(orderHistoryPanelLayout);
+        orderHistoryPanelLayout.setHorizontalGroup(
+            orderHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(orderHistoryPanelLayout.createSequentialGroup()
+                .addGroup(orderHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(orderHistoryPanelLayout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(jLabel4)
+                        .addGap(189, 189, 189)
+                        .addComponent(jLabel5))
+                    .addGroup(orderHistoryPanelLayout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(75, 75, 75)
+                        .addComponent(reviewsList, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(orderHistoryPanelLayout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(reorderButton)))
+                .addContainerGap(264, Short.MAX_VALUE))
+        );
+        orderHistoryPanelLayout.setVerticalGroup(
+            orderHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(orderHistoryPanelLayout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addGroup(orderHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addGap(18, 18, 18)
+                .addGroup(orderHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
+                    .addComponent(reviewsList, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(35, 35, 35)
+                .addComponent(reorderButton)
+                .addContainerGap(58, Short.MAX_VALUE))
+        );
+
+        mainPanel.addTab("Orders", orderHistoryPanel);
+
+        complainsTextArea.setColumns(20);
+        complainsTextArea.setRows(5);
+        complains.setViewportView(complainsTextArea);
+
+        jLabel6.setText("Write your complain");
+
+        submitComplain.setText("Submit");
+
+        javax.swing.GroupLayout complainsPanelLayout = new javax.swing.GroupLayout(complainsPanel);
+        complainsPanel.setLayout(complainsPanelLayout);
+        complainsPanelLayout.setHorizontalGroup(
+            complainsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(complainsPanelLayout.createSequentialGroup()
+                .addGroup(complainsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(complainsPanelLayout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addGroup(complainsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(complains, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(complainsPanelLayout.createSequentialGroup()
+                        .addGap(111, 111, 111)
+                        .addComponent(submitComplain)))
+                .addContainerGap(466, Short.MAX_VALUE))
+        );
+        complainsPanelLayout.setVerticalGroup(
+            complainsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(complainsPanelLayout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(complains, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(submitComplain)
+                .addContainerGap(116, Short.MAX_VALUE))
+        );
+
+        mainPanel.addTab("Complains", complainsPanel);
+
+        transactionTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null}
+            },
+            new String [] {
+                "null", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        transactionTable.setColumnSelectionAllowed(true);
+        jScrollPane3.setViewportView(transactionTable);
+
+        jLabel7.setText("Transaction History");
+
+        javax.swing.GroupLayout transactionHistoryPanelLayout = new javax.swing.GroupLayout(transactionHistoryPanel);
+        transactionHistoryPanel.setLayout(transactionHistoryPanelLayout);
+        transactionHistoryPanelLayout.setHorizontalGroup(
+            transactionHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(transactionHistoryPanelLayout.createSequentialGroup()
+                .addGroup(transactionHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(transactionHistoryPanelLayout.createSequentialGroup()
+                        .addGap(96, 96, 96)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 467, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(transactionHistoryPanelLayout.createSequentialGroup()
+                        .addGap(240, 240, 240)
+                        .addComponent(jLabel7)))
+                .addContainerGap(172, Short.MAX_VALUE))
+        );
+        transactionHistoryPanelLayout.setVerticalGroup(
+            transactionHistoryPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(transactionHistoryPanelLayout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 324, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        mainPanel.addTab("Transaction History", transactionHistoryPanel);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 2, 24)); // NOI18N
+        jLabel1.setText("Welcome back");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 2, 24)); // NOI18N
+        jLabel2.setText("User");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 624, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(mainPanel)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 437, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(mainPanel))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void placeorderbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_placeorderbuttonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_placeorderbuttonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +317,30 @@ public class CustomerMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane complains;
+    private javax.swing.JPanel complainsPanel;
+    private javax.swing.JTextArea complainsTextArea;
+    private javax.swing.JComboBox<String> deliveryOption;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTabbedPane mainPanel;
+    private javax.swing.JList<String> menuList;
+    private javax.swing.JPanel menuPanel;
+    private javax.swing.JList<String> orderHistoryList;
+    private javax.swing.JPanel orderHistoryPanel;
+    private javax.swing.JButton placeorderbutton;
+    private javax.swing.JButton reorderButton;
+    private javax.swing.JList<String> reviewsList;
+    private javax.swing.JButton submitComplain;
+    private javax.swing.JPanel transactionHistoryPanel;
+    private javax.swing.JTable transactionTable;
     // End of variables declaration//GEN-END:variables
 }
